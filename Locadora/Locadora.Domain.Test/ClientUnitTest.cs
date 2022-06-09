ff69109ce7632c5ce04763cb829a1d72fc39bca0
Danilo <danilofgama@gmail.com>
2022-06-09 17:11:22 -0300

    Add Domain Tests

---
+++ Locadora/Locadora.Domain.Test/ClientUnitTest.cs
@@ -0,0 +1,63 @@
+﻿using FluentAssertions;
+using Locadora.Domain.Entities;
+using Locadora.Domain.Validation;
+using System;
+using Xunit;
+
+namespace Locadora.Domain.Test
+{
+    public class ClientUnitTest
+    {
+        [Fact]
+        public void CreateClient_WithValidParameters()
+        {
+            Action action = () => new Client(1, "Client Name", 123546);
+            action.Should()
+                .NotThrow<DomainExceptionValidation>();
+        }
+
+        [Fact]
+        public void CreateClient_NegativeIdValue()
+        {
+            Action action = () => new Client(-1, "Client Name", 123546);
+            action.Should()
+                .Throw<DomainExceptionValidation>()
+                .WithMessage("Invalid id.");
+        }
+
+        [Fact]
+        public void CreateClient_NegativeDocumentValue()
+        {
+            Action action = () => new Client(1, "Client Name", -1);
+            action.Should()
+                .Throw<DomainExceptionValidation>()
+                .WithMessage("Invalid document.");
+        }
+
+        [Fact]
+        public void CreateClient_ShortNameValue()
+        {
+            Action action = () => new Client(1, "Cl", 123546);
+            action.Should()
+                .Throw<DomainExceptionValidation>()
+                .WithMessage("Invalid name, too short, minimum 3 characters.");
+        }
+
+        [Fact]
+        public void CreateClient_MissingNameValue()
+        {
+            Action action = () => new Client(1, "", 123546);
+            action.Should()
+                .Throw<DomainExceptionValidation>()
+                .WithMessage("Invalid name. Name is required.");
+        }
+
+        [Fact]
+        public void CreateClient_WithNullNameValue()
+        {
+            Action action = () => new Client(1, null, 123456);
+            action.Should()
+                .Throw<DomainExceptionValidation>();
+        }
+    }
+}
---
+++ Locadora/Locadora.Domain.Test/FilmUnitTest.cs
@@ -0,0 +1,63 @@
+﻿using FluentAssertions;
+using Locadora.Domain.Entities;
+using Locadora.Domain.Validation;
+using System;
+using Xunit;
+
+namespace Locadora.Domain.Test
+{
+    public class FilmUnitTest
+    {
+        [Fact]
+        public void CreateFilm_WithValidParameters()
+        {
+            Action action = () => new Film(1, "Film Title", 1);
+            action.Should()
+                .NotThrow<DomainExceptionValidation>();
+        }
+
+        [Fact]
+        public void CreateFilm_NegativeIdValue()
+        {
+            Action action = () => new Film(-1, "Film Title", 1);
+            action.Should()
+                .Throw<DomainExceptionValidation>()
+                .WithMessage("Invalid id.");
+        }
+
+        [Fact]
+        public void CreateFilm_NegativeStockValue()
+        {
+            Action action = () => new Film(1, "Film Title", -1);
+            action.Should()
+                .Throw<DomainExceptionValidation>()
+                .WithMessage("Invalid stock.");
+        }
+
+        [Fact]
+        public void CreateFilm_ShortTitleValue()
+        {
+            Action action = () => new Film(1, "Fi", 1);
+            action.Should()
+                .Throw<DomainExceptionValidation>()
+                .WithMessage("Invalid title, too short, minimun 3 characters.");
+        }
+
+        [Fact]
+        public void CreateFilm_MissingTitleValue()
+        {
+            Action action = () => new Film(1, "", 1);
+            action.Should()
+                .Throw<DomainExceptionValidation>()
+                .WithMessage("Invalid title. Title is required.");
+        }
+
+        [Fact]
+        public void CreateFilm_WithNullTitleValue()
+        {
+            Action action = () => new Film(1, null, 1);
+            action.Should()
+                .Throw<DomainExceptionValidation>();
+        }
+    }
+}
