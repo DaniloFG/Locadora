741aec5383f8bef554fc6337b325b4d030bdca19
Danilo <danilofgama@gmail.com>
2022-06-10 14:40:21 -0300

    Add infra ioc

---
+++ Locadora/Locadora.InfraIoC/DependencyInjectionAPI.cs
@@ -0,0 +1,25 @@
+﻿using Locadora.Domain.Interfaces;
+using Locadora.InfraData.Context;
+using Locadora.InfraData.Repository;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Locadora.InfraIoC
+{
+    public static class DependencyInjectionAPI
+    {
+        public static IServiceCollection AddInfrastructureAPI(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"),
+                 b => b.MigrationsAssembly(typeof(AppDbContext).Assembly.FullName)));
+
+            services.AddScoped<IClientRepository, ClientRepository>();
+            services.AddScoped<IFilmRepository, FilmRepository>();
+            services.AddScoped<IRentRepository, RentRepository>();
+
+            return services;
+        }
+    }
+}
